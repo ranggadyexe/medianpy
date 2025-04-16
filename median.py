de49f4709a4ecee9b770967fbeaafd7c245bc7ce
Rangga Dewa <ranggadewa853@gmail.com>
2025-04-16 08:50:01 +0700

    masukin kode

---
+++ median.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Data
+data = [77, 76, 77, 79, 74, 80, 78]
+
+# Menghitung median
+median = np.median(data)
+print("Harga median:", median)
+
+# Rangga Dewa Yudhistira 227064516098
